e321bd070115c9390813c0c9b3855aee6ea7f327
David Stichter <david.stichter3414@gmail.com>
2016-02-10 21:12:36 -0500

    Working update

--- wishlist.js
+++ wishlist.js
@@ -3,7 +3,7 @@ var app = express()
 var expressHandlebars = require('express-handlebars')
 var mysql = require('mysql')
 var bodyParser = require('body-parser')
-
+app.use(bodyParser.urlencoded({extended: false}));
 var PORT = 8000;
 app.engine('handlebars', expressHandlebars({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
@@ -25,15 +25,24 @@ app.get('/',function(req,res){
   })
 })
 app.post('/',function(req,res){
-  console.log(req.body)
-  // var query = 'INSERT INTO wishlist(item) VALUES '+ req.body.wishData
-  // connection.query(query,function(err,results){
-  //   res.redirect('/')
-  // })
-})
-app.delete('/',function(req,res){
-  var query = 'INSERT INTO wishlist(item) VALUES '+ req.body.note
+  var query = 'INSERT INTO wishlist (item) VALUES ("'+ req.body.wishData + '");'
+  console.log(query)
   connection.query(query,function(err,results){
+    if(err) throw err
+    res.redirect('/')
+  })
+})
+app.get('/delete/:id',function(req,res){
+  var query = 'DELETE FROM wishlist WHERE id=' + req.params.id
+  connection.query(query,function(err,results){
+    res.redirect('/')
+  })
+})
+app.post('/update/:id',function(req,res){
+  var query = 'UPDATE wishlist SET item="'+ req.body.update + '" WHERE id=' + req.params.id + ';'
+  console.log(query)
+  connection.query(query,function(err,results){
+    if(err) throw err
     res.redirect('/')
   })
 })
